bc934ce08ab10566cdabec5a90726fead612faac
Dan Yeaw <dan@yeaw.me>
2026-07-15 16:44:58 -0400

    Update local test channel with new pipeline

--- tests/conda_local_channel/generate_noarch_wheel_repodata.py
+++ tests/conda_local_channel/generate_noarch_wheel_repodata.py
@@ -6,6 +6,7 @@ from the packages listed in ``wheel_packages.txt``. Not intended for production 
 """
 
 import logging
+from typing import Any
 
 import requests
 from conda_index.index import BaseCondaIndexCache, ChannelIndex
@@ -17,17 +18,21 @@ from conda_pypi.index import store_pypi_metadata
 log = logging.getLogger(__name__)
 
 
-def cache_repodata_entry(cache: BaseCondaIndexCache, name: str, version: str):
+def cache_repodata_entry(
+    cache: BaseCondaIndexCache, name: str, version: str
+) -> dict[str, Any] | None:
     pypi_endpoint = f"https://pypi.org/pypi/{name}/{version}/json"
     pypi_data = requests.get(pypi_endpoint)
     if pypi_data.json() is None:
         log.error(f"unable to process {name} {version}, no data found at {pypi_endpoint}")
+        return None
     try:
-        store_pypi_metadata(cache, pypi_data.json())
+        return store_pypi_metadata(cache, pypi_data.json())
     except UnableToConvertToRepodataEntry:
         log.error(
             f"unable to process {name} {version}, unable to convert pypi metadata to a repodata entry"
         )
+        return None
 
 
 if __name__ == "__main__":
@@ -54,11 +59,20 @@ if __name__ == "__main__":
         repodata_v3=True,
         update_only=True,
         save_fs_state=False,
-        cache_kwargs={"package_extensions": CONDA_PACKAGE_EXTENSIONS + (".whl",)},
+        cache_kwargs={
+            "package_extensions": CONDA_PACKAGE_EXTENSIONS + (".whl",),
+            "include_stages": ["md"],
+        },
     )
     cache = channel_index.cache_for_subdir("noarch")
 
+    md_entries = []
     for pkg_tuple in repodata_packages:
-        cache_repodata_entry(cache, pkg_tuple[0], pkg_tuple[1])
+        entry = cache_repodata_entry(cache, pkg_tuple[0], pkg_tuple[1])
+        if entry is not None:
+            md_entries.append(entry)
+
+    if md_entries:
+        cache.store_stat_state("md", md_entries)
 
     channel_index.index(patch_generator=None)
